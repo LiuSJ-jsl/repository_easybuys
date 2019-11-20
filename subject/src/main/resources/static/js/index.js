58dd81f2119fecad1376fb00293215227337d9b6
zhang123-java <858074957@qq.com>
2019-11-20 10:18:19 +0800

    易买网项目测试z

--- subject/src/main/resources/static/js/index.js
+++ subject/src/main/resources/static/js/index.js
@@ -2,6 +2,9 @@ jQuery(function() {
     var userName = jQuery(".username").val();
 
     if (userName =="null" || userName == ""){
+
+
+
         alert(userName)
         jQuery(".sou").append(' <span class="fl">你好，请<a href="/login">登录</a>&nbsp; <a href="regist.html" style="color:#ff4e00;">免费注册</a>&nbsp;|&nbsp;<a href="#">我的订单</a>&nbsp;|</span>');
     }else {
