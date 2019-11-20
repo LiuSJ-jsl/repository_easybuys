589cde3c5dbc1843ba44b2f62003d3d7a1118dc2
zhang123-java <858074957@qq.com>
2019-11-20 13:50:10 +0800

    易买网项z

--- seckill2/src/main/java/com/htkj/seckill2/service/impl/OrderServiceImpl.java
+++ seckill2/src/main/java/com/htkj/seckill2/service/impl/OrderServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
--- subject/src/main/resources/static/js/index.js
+++ subject/src/main/resources/static/js/index.js
@@ -6,7 +6,7 @@ jQuery(function() {
         jQuery(".sou").prepend(' <span class="fl">你好，请<a href="/login">登录</a>&nbsp; <a href="regist.html" style="color:#ff4e00;">免费注册</a>&nbsp;|&nbsp;<a href="#">我的订单</a>&nbsp;|</span>');
         jQuery(".car_bg").prepend('<div class="un_login">还未登录！<a href="login.html" style="color:#ff4e00;">马上登录</a> 查看购物车！</div>\n')
     }else {
-        jQuery(".car_bg").padding(' <ul class="cars">\n' +
+        jQuery(".car_bg").prepend(' <ul class="cars">\n' +
             '                        <li>\n' +
             '                            <div class="img"><a href="#"><img src="/static/images/car1.jpg" width="58" height="58"/></a>\n' +
             '                            </div>\n' +
@@ -18,13 +18,7 @@ jQuery(function() {
             '                            </div>\n' +
             '                            <div class="name"><a href="#">香奈儿（Chanel）邂逅活力淡香水50ml</a></div>\n' +
             '                            <div class="price"><font color="#ff4e00">￥399</font> X1</div>\n' +
-            '                        </li>\n' +
-            '                        <li>\n' +
-            '                            <div class="img"><a href="#"><img src="/static/images/car2.jpg" width="58" height="58"/></a>\n' +
-            '                            </div>\n' +
-            '                            <div class="name"><a href="#">香奈儿（Chanel）邂逅活力淡香水50ml</a></div>\n' +
-            '                            <div class="price"><font color="#ff4e00">￥399</font> X1</div>\n' +
-            '                        </li>\n' +
+            '                        </li>\n'+
             '                    </ul>\n' +
             '                    <div class="price_sum">共计&nbsp; <font color="#ff4e00">￥</font><span>1058</span></div>\n' +
             '                    <div class="price_a"><a th:href="@{/buycar}">去购物车结算</a></div>\n' +
